a84629a09729f6ae7ab9847191928cf4c94887f5
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-08-16 00:05:14 +0100

    new regression data structure

--- regression_data.py
+++ regression_data.py
@@ -1,87 +1,159 @@
-import json
-import pandas as pd
-from pathlib import Path
-import shutil
-import os
-import dominate
-from datetime import datetime
+import json as _json
 
+class test_entry_store:
+    def __init__(self):
+        self.entries = []
 
-def load_regression_store(filename = "./regression_data.dat"):
-    '''Load output from pytest and return as pandas dataframe'''
-    with open(filename, "r") as f:
-        data = json.load(f)
+    def append(self, entry):
+        self.entries.append(entry)
 
-    return pd.DataFrame(data)
+    def __len___(self):
+        return len(self.entries)
 
-def copy_regression_data(filename = "./regression_data.dat",
-                         destination = "../regression_data/data/"):
-    '''Loop over output from pytest and copy files to destination (usually repository for regression output'''
-    df = load_regression_store()
+    def write_json(self, file_name):
+        with open(file_name, "w") as f:
+            f.write("[")
+            for i, entry in enumerate(self.entries):
+                _json.dump(entry.to_dict(), f)
+                if i != len(self.entries)-1 :
+                    f.write(",\n")
+                else :
+                    f.write("\n")
+            f.write("]")
 
-    # copy over configuration file
-    shutil.copy2(filename, destination)
+    def read_json(self, file_name):
+        with open(file_name, "r") as f:
+            d = _json.load(f)
+            self.from_dict(d)
 
-    # copy over all requested output files
-    icopied = 0
-    for index, row in df.iterrows():
-        testname = row['testname']
-        testfile = row['testfile']
-        testfiletype = row['testtfiletype']
+    def from_dict(self, d):
+        # loop over entries
 
-        testdir = testname.split("/")[0]
-        filepath = Path("./"+testdir+"/"+testfile)
-        destpath = Path(destination+"/"+testdir+"/")
-        destfilepath = destpath / Path(testfile)
+        self.entries = []
+        for e in d :
+            et = test_entry()
+            et.from_dict(e)
+            self.append(et)
 
-        # create output directory if does not exist
-        if not destpath.exists() :
-            destpath.mkdir()
+    def __repr__(self):
+        s = "["
+        for e in self.entries :
+            s += repr(e) + ","
 
-        # copy file
-        shutil.copy2(filepath, destfilepath)
+        s += "]"
+        return s
 
-        icopied += 1
+class test_entry:
+    def __init__(self, test_name = None, test_file_path = None, nprimary = 0, runtime=0):
+        self.name = test_name
+        self.file_path = test_file_path
+        self.nprimary = nprimary
+        self.runtime = runtime
+        self.input_parameters = []
+        self.output_parameters = []
+        self.output_files = []
 
-    print(f"Copied : {icopied} to {destination}")
+    def add_input_parameter(self, parameter_name, parameter_value):
+        self.input_parameters.append(test_input_parameter(parameter_name, parameter_value))
 
-def compare_regression_data(path1 = "./", path2 = None, html_path="./html"):
-    from dominate.tags import table, tr, th, td, title, h1, link, script
+    def add_input_parameter_dict(self, pdict):
+        for k in pdict:
+            self.add_input_parameter(k, pdict[k])
 
-    df = load_regression_store(path1+"/regression_data.dat")
+    def add_output_parameter(self, parameter_name, parameter_value):
+        self.output_parameters.append(test_output_parameter(parameter_name, parameter_value))
 
+    def add_output_parameter_dict(self, pdict):
+        for k in pdict:
+            self.add_output_parameter(k, pdict[k])
 
-    Path(html_path).mkdir(parents=True, exist_ok=True)
+    def add_output_file(self, file_path, file_type):
+        self.output_files.append(test_output_file(file_path, file_type))
 
-    doc = dominate.document(title='Regression tests')
+    def add_output_file_dict(self, fdict):
+        for k in fdict:
+            self.add_output_file(k, fdict[k])
+            
+    def from_dict(self, d):
+        self.name = d["name"]
+        self.file_path = d["file_path"]
+        self.nprimary = d["nprimary"]
+        self.runtime = d["runtime"]
+        for v in d["input_parameters"]:
+            p = test_input_parameter(None, None)
+            p.from_dict(v)
+            self.input_parameters.append(p)
 
-    columns_display = ['testname', 'testfile', 'testfiletype',
-                       'testfilesize', 'testobject','testnprimary']
+        for v in d["output_parameters"]:
+            o = test_output_parameter(None, None)
+            o.from_dict(v)
+            self.output_parameters.append(o)
 
-    with doc:
-        with doc.head:
-            link(rel="stylesheet", href="styles.css")
-            script(src="script.js")
+        for v in d["output_files"]:
+            f = test_output_file(None, None)
+            f.from_dict(v)
+            self.output_files.append(f)
 
-        # page title
-        now = datetime.now()
-        h1("Regression tests (" + now.strftime("%Y-%m-%d %H:%M:%S") +")" )
+    def to_dict(self):
+        d = {
+            "name": self.name,
+            "file_path": self.file_path,
+            "nprimary": self.nprimary,
+            "runtime": self.runtime,
+            "input_parameters": [p.to_dict() for p in self.input_parameters],
+            "output_parameters": [o.to_dict() for o in self.output_parameters],
+            "output_files": [o.to_dict() for o in self.output_files]
+        }
+        return d
 
-        # loop over regression data frame
-        with table(border="1"):
-            with tr():
-                for h in columns_display:
-                    th(h)
-            for index, row in df.iterrows():
-                # if optics file make pdf
+    def __repr__(self):
+        s =  f"test_entry(name={self.name}, file_path={self.file_path}, nprimary={self.nprimary}\n"
+        s += f"input_parameters={repr(self.input_parameters)}\n"
+        s += f"output_parameters={repr(self.output_parameters)}\n"
+        s += f"output_files={repr(self.output_files)})"
+        return s
 
-                # if rebdsim file make pdf
+class test_input_parameter:
+    def __init__(self, parameter_name, parameter_value):
+        self.parameter_name = parameter_name
+        self.parameter_value = parameter_value
 
-                # make table row
-                with tr():
-                    for column, cell in zip(df.columns,row):
-                        if column in columns_display:
-                            td(str(cell))
+    def to_dict(self):
+        return {"parameter_name": self.parameter_name, "parameter_value": self.parameter_value}
 
-    with open(html_path+"/regression_data.html", "w") as f:
-        f.write(str(doc))
+    def from_dict(self, d):
+        self.parameter_name = d["parameter_name"]
+        self.parameter_value = d["parameter_value"]
+
+    def __repr__(self):
+        return f"test_input_parameter(name={self.parameter_name}, value={self.parameter_value})"
+
+class test_output_parameter:
+    def __init__(self, parameter_name, parameter_value):
+        self.parameter_name = parameter_name
+        self.parameter_value = parameter_value
+
+    def from_dict(self, d):
+        self.parameter_name = d["parameter_name"]
+        self.parameter_value = d["parameter_value"]
+
+    def to_dict(self):
+        return {"parameter_name": self.parameter_name, "parameter_value": self.parameter_value}
+
+    def __repr__(self):
+        return f"test_output_parameter(name={self.parameter_name}, value={self.parameter_value})"
+
+class test_output_file:
+    def __init__(self, file_path=None, file_type=None):
+        self.file_path = file_path
+        self.file_type = file_type
+
+    def to_dict(self):
+        return {"file_path": self.file_path, "file_type": self.file_type}
+
+    def from_dict(self, d):
+        self.file_path = d["file_path"]
+        self.file_type = d["file_type"]
+
+    def __repr__(self):
+        return f"test_output_file(file_path={self.file_path}, file_type={self.file_type})"
