ecab8dad90ddddb5ee1a519bc64504f8b44b9e7b
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-08-16 00:37:35 +0100

    type hints

--- regression_data.py
+++ regression_data.py
@@ -1,56 +1,56 @@
 import json as _json
 
-class test_entry_store:
-    def __init__(self):
-        self.entries = []
+class test_input_parameter:
+    def __init__(self, parameter_name : str, parameter_value):
+        self.parameter_name = parameter_name
+        self.parameter_value = parameter_value
 
-    def append(self, entry):
-        self.entries.append(entry)
+    def to_dict(self) -> dict:
+        return {"parameter_name": self.parameter_name, "parameter_value": self.parameter_value}
 
-    def __len__(self):
-        return len(self.entries)
+    def from_dict(self, d : dict ) -> None:
+        self.parameter_name = d["parameter_name"]
+        self.parameter_value = d["parameter_value"]
 
-    def __getitem__(self, index):
-        return self.entries[index]
+    def __repr__(self) -> str:
+        return f"test_input_parameter(name={self.parameter_name}, value={self.parameter_value})"
 
-    def __setitem__(self, index, value):
-        self.entries[index] = value
+class test_output_parameter:
+    def __init__(self, parameter_name : str, parameter_value):
+        self.parameter_name = parameter_name
+        self.parameter_value = parameter_value
 
-    def write_json(self, file_name):
-        with open(file_name, "w") as f:
-            f.write("[")
-            for i, entry in enumerate(self.entries):
-                _json.dump(entry.to_dict(), f)
-                if i != len(self.entries)-1 :
-                    f.write(",\n")
-                else :
-                    f.write("\n")
-            f.write("]")
+    def from_dict(self, d : dict) -> None:
+        self.parameter_name = d["parameter_name"]
+        self.parameter_value = d["parameter_value"]
 
-    def read_json(self, file_name):
-        with open(file_name, "r") as f:
-            d = _json.load(f)
-            self.from_dict(d)
+    def to_dict(self) -> dict:
+        return {"parameter_name": self.parameter_name, "parameter_value": self.parameter_value}
 
-    def from_dict(self, d):
-        # loop over entries
+    def __repr__(self) -> str:
+        return f"test_output_parameter(name={self.parameter_name}, value={self.parameter_value})"
 
-        self.entries = []
-        for e in d :
-            et = test_entry()
-            et.from_dict(e)
-            self.append(et)
+class test_output_file:
+    def __init__(self, file_path : str =None, file_type : str =None):
+        self.file_path = file_path
+        self.file_type = file_type
+
+    def to_dict(self) -> dict:
+        return {"file_path": self.file_path, "file_type": self.file_type}
+
+    def from_dict(self, d : dict) -> None:
+        self.file_path = d["file_path"]
+        self.file_type = d["file_type"]
 
     def __repr__(self):
-        s = "["
-        for e in self.entries :
-            s += repr(e) + ","
-
-        s += "]"
-        return s
+        return f"test_output_file(file_path={self.file_path}, file_type={self.file_type})"
 
 class test_entry:
-    def __init__(self, test_name = None, test_file_path = None, nprimary = 0, runtime=0):
+    def __init__(self,
+                 test_name      : str = None,
+                 test_file_path : str = None,
+                 nprimary       : int = 0,
+                 runtime        : int =0):
         self.name = test_name
         self.file_path = test_file_path
         self.nprimary = nprimary
@@ -59,28 +59,28 @@ class test_entry:
         self.output_parameters = []
         self.output_files = []
 
-    def add_input_parameter(self, parameter_name, parameter_value):
+    def add_input_parameter(self, parameter_name, parameter_value) -> None:
         self.input_parameters.append(test_input_parameter(parameter_name, parameter_value))
 
-    def add_input_parameter_dict(self, pdict):
+    def add_input_parameter_dict(self, pdict) -> None:
         for k in pdict:
             self.add_input_parameter(k, pdict[k])
 
-    def add_output_parameter(self, parameter_name, parameter_value):
+    def add_output_parameter(self, parameter_name, parameter_value) -> None:
         self.output_parameters.append(test_output_parameter(parameter_name, parameter_value))
 
-    def add_output_parameter_dict(self, pdict):
+    def add_output_parameter_dict(self, pdict) -> None:
         for k in pdict:
             self.add_output_parameter(k, pdict[k])
 
-    def add_output_file(self, file_path, file_type):
+    def add_output_file(self, file_path, file_type) -> None:
         self.output_files.append(test_output_file(file_path, file_type))
 
-    def add_output_file_dict(self, fdict):
+    def add_output_file_dict(self, fdict) -> None:
         for k in fdict:
             self.add_output_file(k, fdict[k])
             
-    def from_dict(self, d):
+    def from_dict(self, d) -> None:
         self.name = d["name"]
         self.file_path = d["file_path"]
         self.nprimary = d["nprimary"]
@@ -112,64 +112,72 @@ class test_entry:
         }
         return d
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         s =  f"test_entry(name={self.name}, file_path={self.file_path}, nprimary={self.nprimary}\n"
         s += f"input_parameters={repr(self.input_parameters)}\n"
         s += f"output_parameters={repr(self.output_parameters)}\n"
         s += f"output_files={repr(self.output_files)})"
         return s
 
-class test_input_parameter:
-    def __init__(self, parameter_name, parameter_value):
-        self.parameter_name = parameter_name
-        self.parameter_value = parameter_value
+class test_entry_store:
+    def __init__(self):
+        self.entries = []
 
-    def to_dict(self):
-        return {"parameter_name": self.parameter_name, "parameter_value": self.parameter_value}
+    def append(self, entry : test_entry) -> None:
+        self.entries.append(entry)
 
-    def from_dict(self, d):
-        self.parameter_name = d["parameter_name"]
-        self.parameter_value = d["parameter_value"]
+    def __len__(self) -> int:
+        return len(self.entries)
 
-    def __repr__(self):
-        return f"test_input_parameter(name={self.parameter_name}, value={self.parameter_value})"
+    def __getitem__(self, index : int) -> test_entry:
+        return self.entries[index]
 
-class test_output_parameter:
-    def __init__(self, parameter_name, parameter_value):
-        self.parameter_name = parameter_name
-        self.parameter_value = parameter_value
+    def __setitem__(self, index : int, value) -> None:
+        self.entries[index] = value
 
-    def from_dict(self, d):
-        self.parameter_name = d["parameter_name"]
-        self.parameter_value = d["parameter_value"]
+    def write_json(self, file_name : str) -> None:
+        with open(file_name, "w") as f:
+            f.write("[")
+            for i, entry in enumerate(self.entries):
+                _json.dump(entry.to_dict(), f)
+                if i != len(self.entries)-1 :
+                    f.write(",\n")
+                else :
+                    f.write("\n")
+            f.write("]")
 
-    def to_dict(self):
-        return {"parameter_name": self.parameter_name, "parameter_value": self.parameter_value}
+    def read_json(self, file_name : str) -> None:
+        with open(file_name, "r") as f:
+            d = _json.load(f)
+            self.from_dict(d)
 
-    def __repr__(self):
-        return f"test_output_parameter(name={self.parameter_name}, value={self.parameter_value})"
+    def from_dict(self, d : dict) -> None:
+        # loop over entries
 
-class test_output_file:
-    def __init__(self, file_path=None, file_type=None):
-        self.file_path = file_path
-        self.file_type = file_type
+        self.entries = []
+        for e in d :
+            et = test_entry()
+            et.from_dict(e)
+            self.append(et)
 
-    def to_dict(self):
-        return {"file_path": self.file_path, "file_type": self.file_type}
+    def __repr__(self) -> str:
+        s = "["
+        for e in self.entries :
+            s += repr(e) + ","
 
-    def from_dict(self, d):
-        self.file_path = d["file_path"]
-        self.file_type = d["file_type"]
+        s += "]"
+        return s
 
-    def __repr__(self):
-        return f"test_output_file(file_path={self.file_path}, file_type={self.file_type})"
-
-def copy_regression_data(file_name = "./regression_data.dat",
-                         destination = "../regression_data/data/"):
+def copy_regression_data(file_name   : str   = "./regression_data.dat",
+                         destination : str = "../regression_data/data/") -> None :
     tes = test_entry_store()
     tes.read_json(file_name)
 
-def compare_regression_data(path1  = "./regression_data.dat",
-                            path2 = None,
-                            output_path = None):
+def compare_regression_data(path1       : str  = "./regression_data.dat",
+                            path2       : str  = None,
+                            output_path : str = None) -> None :
     pass
+
+def html_regression_data(path1 : str = "./regression_data.dat") -> None :
+    pass
+
