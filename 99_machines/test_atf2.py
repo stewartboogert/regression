90856af456555eeb32248b1f4db015364a11bab5
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-08-16 15:47:56 +0100

    fix minor copy and paste bug

--- 99_machines/test_atf2.py
+++ 99_machines/test_atf2.py
@@ -17,6 +17,5 @@ def test(test_length, testlength_primaries, testdata_store) :
     pybdsim.Run.RebdsimOptics(root_name,optics_name)
 
     te = testdata_store.new_test_entry(__file__,__file__,nprimary,0)
-    te.add_output_file()
     te.add_output_file(os.path.dirname(__file__)+"/"+optics_name, "optics")
 
--- 99_machines/test_lhc.py
+++ 99_machines/test_lhc.py
@@ -17,5 +17,4 @@ def test(test_length, testlength_primaries, testdata_store):
     pybdsim.Run.RebdsimOptics(root_name, optics_name)
 
     te = testdata_store.new_test_entry(__file__,__file__,nprimary,0)
-    te.add_output_file()
     te.add_output_file(os.path.dirname(__file__)+"/"+optics_name, "optics")
