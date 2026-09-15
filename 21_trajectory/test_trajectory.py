b174dd5162640e82860b894e9e8a1568e1b24426
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-09-15 23:11:31 +0100

    basic trajectory testing

---
+++ 21_trajectory/test_trajectory.py
@@ -0,0 +1,59 @@
+import pytest
+import pybdsim
+import os
+from pathlib import Path
+
+@pytest.mark.parametrize("param, value, pname", [
+    ('SAMPLER_SETTING', '', 'samplenone'),
+    ('SAMPLER_SETTING', 'sample, all;','sampleall'),
+    ('PHYSICS_LISTS', 'physicsList="em",','physicsList_em')
+])
+def test(geant4_version, bdsim_version,
+         test_length, testlength_primaries, testdata_store,
+         param, value, pname) :
+
+    os.chdir(Path(__file__).resolve().parent)
+
+    base_name     = "trajectory"+"_"+pname
+    template_name = "trajectory.tpl"
+    gmad_name     = base_name+".gmad"
+    root_name     = base_name+".root"
+
+    ngenerate = 1
+
+    params = {'SAMPLER_SETTING':'',
+              'PHYSICS_LISTS':''}
+
+    # set parametrised value
+    params[param] = value
+
+    pybdsim.Run.RenderGmadJinjaTemplate(template_name,gmad_name,params)
+    pybdsim.Run.Bdsim(gmad_name,base_name,ngenerate,1)
+    data = pybdsim.DataPandas.BDSIMOutput(root_name)
+
+    # event structure
+    e = data.get_events()
+    ntraj = e.iloc[0]['ntraj']
+
+    # find primary trajectory
+    trajectories = data.get_trajectories(0)
+    iprimary = trajectories[trajectories['parentID'] == 0].index.tolist()[0]
+
+    # assert on primary trajectory
+    t, ids = data.get_trajectory(0, iprimary)
+
+    if pname == "samplenone" :
+        assert(ntraj == 1)
+        assert(len(t) == 14)
+    elif pname == "sampleall" :
+        assert(ntraj == 1)
+        assert(len(t) == 20)
+    elif pname == "physicsList_em" :
+        assert(ntraj == 4044)
+        assert(len(t) == 72)
+
+    # store output parameters for regression testing
+    te = testdata_store.new_test_entry("21_trajectory/trajectory"+"_"+pname, __file__, ngenerate, 0)
+    te.add_input_parameter_dict(params)
+    te.add_output_parameter("ntraj", int(ntraj))
+    te.add_output_parameter("len(trajectory)",len(t))
---
+++ 21_trajectory/trajectory.tpl
@@ -0,0 +1,22 @@
+d1: drift, l=1*m;
+c1: rcol, l=5*cm, xsize=0, ysize=0, material="G4_Fe";
+d2: drift, l=1*m;
+
+l0 : line = (d1,c1,d2);
+
+use, period=l0;
+
+{{ SAMPLER_SETTING }}
+
+beam, particle="e-",
+      energy=10*GeV,
+      X0=0.0*m,
+      Xp0=0.0,
+      Y0=0.0*m,
+      Yp0=0.0,
+      distrType="reference";
+
+option, {{ PHYSICS_LISTS }}
+        seed=1,
+        storeTrajectory=1,
+        storeTrajectoryDepth=-1;
