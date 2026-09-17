2555630b3b68713fd96a42be6c912eeeef475473
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-09-17 19:41:56 +0100

    parentID is nullptr in C++ and gives an incorrect value in python.

--- 21_trajectory/trajectory_analysis.py
+++ 21_trajectory/trajectory_analysis.py
@@ -9,6 +9,10 @@ def build_topdown_tree(t) :
 
     # loop over all trajectories
     for itraj in range(ntraj) :
+
+        if t.parentID[itraj] == 0 :
+            continue
+
         parent_index = t.parentIndex[itraj]
         parent_step  = t.parentStepIndex[itraj]
         daughter_index[parent_index].append(itraj)
@@ -29,5 +33,14 @@ def build_topdown_tree(t) :
 def hash_trajectory(t, itraj) :
     pass
 
+def visit_trajectories(t, daugher_index, daughter_step, itraj_start, traj_hash_func = None) :
+    pass
+
+
 def traverse_trajectories(t, traj_hash_func = None) :
     daughter_index, daughter_step = build_topdown_tree(t)
+
+    # recurse down tree
+    visit_trajectories(t, daughter_index, daughter_step, traj_hash_func)
+
+
